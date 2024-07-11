c83c90fc7c358b6c8bedb687f0e53963c11d280b
Amir Shahbabaee <amirhossein.shahbabaee@cleura.com>
2024-07-11 10:56:05 +0200

    1-Setup project and test

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <div className="App">A</div>;
+}
+
+export default App;
---
+++ src/sample.test.ts
@@ -0,0 +1,5 @@
+describe("sample test", () => {
+  it("test 1", () => {
+    expect(true).toStrictEqual(true);
+  });
+});
